ed899b8fa1ef2a1c3c2b9bc71f5263e4005fe3f7
David <David.Piruzashvili@gmail.com>
2023-10-05 13:58:21 +0400

    Create tabs another version with Function

--- js/script.js
+++ js/script.js
@@ -4,20 +4,53 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+          tabsParent = document.querySelector('.tabheader__items'),
+          tabsItem = document.querySelectorAll('.tabheader__item');
+
+    // tabsParent.addEventListener('click', (e) => {
+    //     if (e.target && e.target.classList.contains('tabheader__item')) {
+    //         document.querySelectorAll('.tabheader__item').forEach((item, i) => {
+    //             item.setAttribute('index', i);
+    //             item.classList.remove('tabheader__item_active');
+    //             tabsContent[i].classList.remove('tabcontent_visible');
+    //         });
+    //         e.target.classList.add('tabheader__item_active');
+    //         let index = e.target.getAttribute('index');
+    //         tabsContent[index].classList.add('tabcontent_visible', 'fade');
+
+    //     }
+
+    // });
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabsItem.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabsItem[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
 
     tabsParent.addEventListener('click', (e) => {
-        if (e.target && e.target.classList.contains('tabheader__item')) {
-            document.querySelectorAll('.tabheader__item').forEach((item, i) => {
-                item.setAttribute('index', i);
-                item.classList.remove('tabheader__item_active');
-                tabsContent[i].classList.remove('tabcontent_visible');
+        const target = e.target;
+        if(target && target.classList.contains('tabheader__item')) {
+            tabsItem.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
             });
-            e.target.classList.add('tabheader__item_active');
-            let index = e.target.getAttribute('index');
-            tabsContent[index].classList.add('tabcontent_visible', 'fade');
-
         }
-
     });
 });
