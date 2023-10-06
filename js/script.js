2b1100a0664a9bf8dfe378aa21058dc436ac8433
David <David.Piruzashvili@gmail.com>
2023-10-06 18:54:23 +0400

    Open modal onScroll & setTimeout

--- js/script.js
+++ js/script.js
@@ -119,4 +119,62 @@ document.addEventListener('DOMContentLoaded', () => {
     setClock('.timer', deadLine);
 
 
+
+    // Modal
+
+    const modalBtn = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
+
+
+
+    function openModal() {
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        clearTimeout(openModalAuto);
+    }
+
+    modalBtn.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            if ( e.target === btn) {
+                openModal();
+            }
+        });
+    });
+
+
+
+
+    function closeModal() {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+
+    modalClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && window.getComputedStyle(modal).display === 'block') {
+            closeModal();
+        }
+    });
+
+    const openModalAuto = setTimeout(openModal, 3000);
+
+   function showModalOnScroll() {
+    const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+    if(window.scrollY >= scrollableHeight -1) {
+       openModal();
+       window.removeEventListener('scroll', showModalOnScroll);
+
+    }
+   }
+
+   window.addEventListener('scroll', showModalOnScroll);
+
 });
