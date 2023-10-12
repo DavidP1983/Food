89e8589b05304680a6b9bd718c0d00332a5dceac
David <David.Piruzashvili@gmail.com>
2023-10-12 12:27:27 +0400

    Fetch API request

--- js/script.js
+++ js/script.js
@@ -267,9 +267,6 @@ document.addEventListener('DOMContentLoaded', () => {
             statusMessage.style.cssText = 'display: block; margin: 0 auto';
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
 
             const formData = new FormData(form);
             
@@ -278,18 +275,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 obj[key] = value;
             }
 
-            const json = JSON.stringify(obj);
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                }else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(obj)
+            })
+            .then((response) => response.text())
+            .then((json) => {
+                console.log(json);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
 
         });
